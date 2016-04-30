cca278c96f0311332141f52cf00c91ee9dac5a7e
jmleroux <jmleroux.pro@gmail.com>
2016-05-01 00:23:21 +0200

    validation in form

--- Form/EnhancedCategoryType.php
+++ Form/EnhancedCategoryType.php
@@ -6,6 +6,8 @@ use Akeneo\Bundle\EnhancedEntityBundle\Entity\EnhancedCategory;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class EnhancedCategoryType extends AbstractType
 {
@@ -20,7 +22,12 @@ class EnhancedCategoryType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('description', 'text');
+        $builder->add('description', 'text', [
+            'constraints' => [
+                new NotBlank(),
+                new Length(['min' => 3, 'max' => 100]),
+            ],
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
